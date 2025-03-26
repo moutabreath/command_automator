72ad0f593971fd6669c95f77327cac3ee4d42eb8
moutabreath <tdruck@gmail.com>
2025-03-26 11:27:27 +0200

    fix fast_copy

--- actionables/cmd/fast_copy.cmd
+++ actionables/cmd/fast_copy.cmd
@@ -1,4 +1,4 @@
 cd C:\Windows\System32
 set source=%1%
 set destination=%2%
-robocopy %source% %destination% /S /E /MT:16 /NFL /NDL /NJH /NJSs
+robocopy %source% %destination% /S /E /MT:16 /NFL /NDL /NJH /NJS
