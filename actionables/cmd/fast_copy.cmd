80dd1b85297ce18a3b392aa818ff1409dde19633
moutabreath <tdruck@gmail.com>
2025-04-04 18:00:31 +0300

    update scripts for replace chars and parenthesis

--- actionables/cmd/fast_copy.cmd
+++ actionables/cmd/fast_copy.cmd
@@ -1,4 +1,7 @@
-cd C:\Windows\System32
 set source=%1%
 set destination=%2%
-robocopy %source% %destination% /S /E /MT:16 /NFL /NDL /NJH /NJS
+if exist "%source%" (
+    xcopy "%source%" "%destination%"
+) else (
+    robocopy "%source%" "%destination%" /S /E /MT:16 /NFL /NDL /NJH /NJS
+)
--- actionables/python/replace_parenthesis.py
+++ actionables/python/replace_parenthesis.py
@@ -2,16 +2,29 @@
 import sys
 
 
-def run_replace_string():
-    if len(sys.argv) <= 3:
-        print("Usage: python <text>  <original char> <replace with> <original char> <replace with> ...")
-        sys.exit(1)
-    text = sys.argv[1]    
-
-    for i in range(2, len(sys.argv) - 1, 2):
-        source_to_replace = sys.argv[i]
-        replace_to = sys.argv[i + 1]        
-        text = text.replace(source_to_replace, replace_to)
+def replace_and_remove_parenthesis(text):    
+    old_text = text
+    x = text
+    text = text.replace("[", "{")
+    text = text.replace("]", "}")
+    old_text = old_text.replace("{","[")
+    old_text = old_text.replace("}","]")
+    x = run_remove_parenthesis(text)
+    print(old_text)
     print(text)
+    print(x)
+
+
+def run_remove_parenthesis(text):
+    x = text.replace("{", "")
+    x = x.replace("}", "")
+    x = x.replace("[", "")
+    x = x.replace("]", "")
+
+def run_replace_string():
+    if len(sys.argv) <= 1:
+        print("Usage: python <text> ")
+        sys.exit(1)
+    replace_and_remove_parenthesis(sys.argv[1])
 
 run_replace_string()
