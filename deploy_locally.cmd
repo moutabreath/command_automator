071541532c0bb42bd8153bf5573e931fe153bc2b
moutabreath <tdruck@gmail.com>
2025-07-09 16:13:12 +0300

    hide the python shell using pyinstaller command, not file extension. commands_automator is py not pyw and deploy locally is adjusted

--- deploy_locally.cmd
+++ deploy_locally.cmd
@@ -1,2 +1,2 @@
-pyinstaller command_automator_api.pyw -i ui/resources/Commands_Automator.ico -F --add-data "ui;ui"
+pyinstaller command_automator_api.py -i ui/resources/Commands_Automator.ico -F --add-data "ui;ui" -w
 xcopy dist\command_automator_api.exe .\command_automator_api.exe /y
