b29487b01153f4c9cf925bb8968406be6ec4b991
moutabreath <tdruck@gmail.com>
2025-07-12 16:45:25 +0300

    depllooy script more robust

--- deploy_dev.cmd
+++ deploy_dev.cmd
@@ -5,12 +5,12 @@ taskkill /IM commands_automator_api.exe /F >nul 2>&1
 
 call .\.venv\Scripts\activate
 
-@REM python -m PyInstaller commands_automator_api.py -i ui/resources/Commands_Automator.ico --onedir --add-data "ui;ui" --noconfirm
-python -m PyInstaller  --clean --debug=all commands_automator.spec > build_log.txt 2>&1
+python -m PyInstaller --clean --debug=all commands_automator_api.py -i ui/resources/Commands_Automator.ico --onedir --add-data "ui;ui" --noconfirm  > build_log.txt 2>&1
+@REM python -m PyInstaller  --clean commands_automator_api.spec > build_log.txt 2>&1 -y
 
 call deactivate
 
 xcopy user_scripts dist\commands_automator_api\user_scripts /E /I /Y
 xcopy config dist\commands_automator_api\config /E /I /Y
 
-start "" ".\dist\command_automator_api\command_automator_api.exe"
+start "" ".\dist\commands_automator_api\commands_automator_api.exe"
