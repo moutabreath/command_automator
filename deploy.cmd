4831694f7f0a0f4558b33d6f61986d31ffa9a28e
moutabreath <tdruck@gmail.com>
2025-07-20 15:38:36 +0300

    fix deploy script for dev

--- deploy.cmd
+++ deploy.cmd
@@ -3,13 +3,11 @@
 REM Kill any running instance of the app before copying
 taskkill /IM commands_automator_api.exe /F >nul 2>&1
 
-call .\.venv\Scripts\activate
 
 @REM python -m PyInstaller --clean --debug=all commands_automator_api.py -i ui/resources/Commands_Automator.ico --onedir --add-data "ui;ui" --noconfirm  > build_log.txt 2>&1
 @REM python -m PyInstaller  --clean commands_automator_api.spec > build_log.txt 2>&1 -y
-python -m PyInstaller commands_automator_api.py -i ui/resources/Commands_Automator.ico -F --add-data "ui;ui" -w
+.\.venv\Scripts\python.exe -m PyInstaller commands_automator_api.py -i ui/resources/Commands_Automator.ico -F --add-data "ui;ui" -w
 
-call deactivate
 
 xcopy "dist\commands_automator_api.exe" "commands_automator_api.exe" /Y
 
