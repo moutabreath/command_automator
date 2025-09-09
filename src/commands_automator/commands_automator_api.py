8b45954f4862e90c1b111029a674182cf8bae732
moutabreath <tdruck@gmail.com>
2025-09-09 15:55:57 +0300

    change log format

--- src/commands_automator/commands_automator_api.py
+++ src/commands_automator/commands_automator_api.py
@@ -66,7 +66,7 @@ class CommandsAutomatorApi:
     def init_logger(self):
         handler = logging.handlers.WatchedFileHandler(
         os.environ.get("LOGFILE", "commands_automator.log"))
-        formatter =logging.Formatter("%(asctime)s:%(name)s:%(levelname)s {%(module)s %(funcName)s}:%(message)s")
+        formatter =logging.Formatter("%(asctime)s: %(name)s: %(levelname)s {%(module)s %(funcName)s}:%(message)s")
         handler.setFormatter(formatter)
         root = logging.getLogger()
         root.setLevel(os.environ.get("LOGLEVEL", "DEBUG"))
