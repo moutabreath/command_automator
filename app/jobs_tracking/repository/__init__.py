0de2be90fa269253832f91a3b8480c5d9669d016
moutabreath <tdruck@gmail.com>
2026-02-05 18:31:41 +0200

    update teh relevant init.py from previous commit

---
+++ app/jobs_tracking/repository/__init__.py
@@ -0,0 +1,6 @@
+from app.jobs_tracking.repository.job_tracking_reader_persist_mongo import CompanyReaderPersistMongo
+from app.jobs_tracking.repository.job_tracking_writer_persist_mongo import JobTrackingWriterPersistMongo
+__all__ = [
+    "CompanyReaderPersistMongo",
+    "JobTrackingWriterPersistMongo"
+]
